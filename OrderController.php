01d7759dd956a59659cf322881cfd33f4381bdd0
PierreTzt <76401152+PierreTzt@users.noreply.github.com>
2023-09-29 14:52:30 +0200

    Update OrderController.php

--- OrderController.php
+++ OrderController.php
@@ -383,13 +383,13 @@ class OrderControllerCore extends FrontController
                 $this->makeLoginForm(),
                 $this->makeCustomerForm()
             ))
-            /*->addStep(new CheckoutAddressesStep(
+            ->addStep(new CheckoutAddressesStep(
                 $this->context,
                 $translator,
                 $this->makeAddressForm()
-            ))*/;
+            ));
 
-        if (!$this->context->cart->isVirtualCart()) {
+        if ($this->context->cart->isVirtualCart()) {
             $checkoutDeliveryStep = new CheckoutDeliveryStep(
                 $this->context,
                 $translator
